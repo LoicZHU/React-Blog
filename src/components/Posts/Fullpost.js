252b9b1699aef894fff5adf3918ff414ff2f83c0
Loïc ZHU <zhu.loic@gmail.com>
2020-09-14 09:42:52 +0200

    Fullpost: fix style

--- src/components/Posts/Fullpost.js
+++ src/components/Posts/Fullpost.js
@@ -8,14 +8,14 @@ const Fullpost = ({ title, category, content }) => (
     <h1 className="main__title">dev of thrones</h1>
 
     <section className="main__article-section">
-      <article className="main__article-section__article">
-        <h2 className="main__article-section__article__title">
+      <article className="main__article-section__link__article">
+        <h2 className="main__article-section__link__article__title">
           {title === 'O’clock, une vraie bonne école ?' ? 'vue, une vraie fausse bonne idée ?' : title}
         </h2>
-        <span className="main__article-section__article__category">
+        <span className="main__article-section__link__article__category">
           {category === ('O’clock' || 'O\'clock') ? 'vue' : category}
         </span>
-        <p className="main__article-section__article__content">
+        <p className="main__article-section__link__article__content">
           {new RegExp('^(O\'clock)|(O’clock)').test(content) ? content.replace(new RegExp('(O\'clock|O’clock).+(une vraie bonne école \\? )'), 'Vue, une vraie fausse bonne idée ? ') : content}
         </p>
       </article>
